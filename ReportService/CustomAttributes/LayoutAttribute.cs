f5083d66d2e1a737e25994a251e75bbfa4de11c6
Werner van Aswegen <18748974+wernerva@users.noreply.github.com>
2019-02-07 20:59:34 +0200

    Added LayputAttribute

---
+++ ReportService/CustomAttributes/LayoutAttribute.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace ReportService.CustomAttributes
+{
+    public class LayoutAttribute: ResultFilterAttribute, IResultFilter
+    {
+        private readonly string _layout;
+
+        public LayoutAttribute(string layout)
+        {
+            if (string.IsNullOrWhiteSpace(layout))
+            {
+                throw new ArgumentException("Invalid layout.");
+            }
+
+            _layout = layout;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            var viewResult = context.Result as ViewResult;
+            if (viewResult != null)
+            {
+                viewResult.ViewData["Layout"] = _layout;
+            }
+        }
+    }
+}
